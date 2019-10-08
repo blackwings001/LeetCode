3b295a1ba4c476d65ff0b331cbebe051b42aa2f8
guominglei <guominglei@zhuanzhuan.com>
2019-10-08 13:05:55 +0800

    61

---
+++ _61_rotateRight.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        # 边界情况
+        if head == None or head.next == None or k <= 0:
+            return head
+
+        #
+
+
+
+if __name__ == '__main__':
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+
+    while head:
+        print("{} -> ".format(head.val), end="")
+        head = head.next
+        if head == None:
+            print(head)
