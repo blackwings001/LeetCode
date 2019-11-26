e401952976dfef214ec0fa0e065ff534c67a133c
guominglei <guominglei@zhuanzhuan.com>
2019-11-26 15:58:38 +0800

    105-110

--- 101-200/_107_levelOrderBottom.py
+++ 101-200/_107_levelOrderBottom.py
@@ -1,9 +1,10 @@
 ﻿# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution(object):
     def levelOrderBottom(self, root):
@@ -11,3 +12,44 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        res = []
+        if not root:
+            return res
+
+        odd_queue = [root]
+        even_queue = []
+
+        while odd_queue or even_queue:
+            tmp = []
+            while odd_queue:
+                ele = odd_queue.pop(0)
+                if ele:
+                    tmp.append(ele.val)
+                    even_queue.append(ele.left)
+                    even_queue.append(ele.right)
+            if tmp:
+                res.append(tmp)
+
+            tmp = []
+            while even_queue:
+                ele = even_queue.pop(0)
+                if ele:
+                    tmp.append(ele.val)
+                    odd_queue.append(ele.left)
+                    odd_queue.append(ele.right)
+            if tmp:
+                res.append(tmp)
+
+        res = [res[i] for i in range(len(res) - 1, -1, -1)]
+        return res
+
+
+if __name__ == '__main__':
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    result = Solution().levelOrderBottom(root)
+    print(result)
--- 101-200/_109_sortedListToBST.py
+++ 101-200/_109_sortedListToBST.py
@@ -1,15 +1,17 @@
 ﻿# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution(object):
     def sortedListToBST(self, head):
@@ -17,3 +19,30 @@ class Solution(object):
         :type head: ListNode
         :rtype: TreeNode
         """
+        # 将链表转化为数组，这样就回到了108题
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+
+        if not nums:
+            return None
+
+        def sortedArrayToBST(nums):
+            """
+            :type nums: List[int]
+            :rtype: TreeNode
+            """
+            if not nums:
+                return None
+
+            mid = len(nums) // 2
+
+            node = TreeNode(nums[mid])
+            node.left = sortedArrayToBST(nums[:mid])
+            node.right = sortedArrayToBST(nums[mid + 1:])
+
+            return node
+
+        root = sortedArrayToBST(nums)
+        return root
