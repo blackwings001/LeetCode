08b82e9276467af5a1597d85ff20095ac395e0bd
guominglei <guominglei@zhuanzhuan.com>
2019-12-02 21:38:50 +0800

    111-113

---
+++ 101-200/_111_minDepth.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return 0 if not root else min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+
+if __name__ == '__main__':
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    res = Solution().minDepth(root)
+    print(res)
