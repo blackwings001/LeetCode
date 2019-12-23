56822ba59580b6817f31b7956c68a2cdd7e673e4
guominglei <guominglei@zhuanzhuan.com>
2019-12-23 11:46:19 +0800

    149

---
+++ 101-150/_149_maxPoints.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def maxPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        if not points:
+            return 0
+
+        max_points = 1
+        nums = len(points)
+        for i in range(nums):
+            cur = points[i]
+            cur_points = {"infinite": 1}  # 斜率不存在时的直线
+            for j in range(i + 1, nums):
+                p = points[j]
+                if p[0] == cur[0]:
+                    cur_points["infinite"] += 1
+                else:
+                    k = (p[1] - cur[1]) / (p[0] - cur[0])
+                    cur_points[k] = cur_points.get(k, 1) + 1
+            max_points = max(max_points, max(cur_points.values()))
+
+        return max_points
+
+
+if __name__ == '__main__':
+    points = [[1, 1], [1, 2], [2, 2], [3, 3], [4, 4]]
+    res = Solution().maxPoints(points)
+    print(res)
--- 101-200/_149_maxPoints.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def maxPoints(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: int
-        """
