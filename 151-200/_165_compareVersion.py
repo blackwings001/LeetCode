60165c001a8f07938ac139159150766dcea6a98c
guominglei <guominglei@zhuanzhuan.com>
2019-12-29 22:09:06 +0800

    165

--- 151-200/_165_compareVersion.py
+++ 151-200/_165_compareVersion.py
@@ -5,3 +5,25 @@ class Solution(object):
         :type version2: str
         :rtype: int
         """
+        if not version1 or not version2:
+            return 0
+
+        version1 = version1.split(".")
+        version2 = version2.split(".")
+
+        for i in range(max(len(version1), len(version2))):
+            val1 = 0 if i >= len(version1) else int(version1[i])
+            val2 = 0 if i >= len(version2) else int(version2[i])
+
+            if val1 > val2:
+                return 1
+            if val1 < val2:
+                return -1
+        return 0
+
+
+if __name__ == '__main__':
+    version1 = "1.001"
+    version2 = "1.1"
+    res = Solution().compareVersion(version1, version2)
+    print(res)
